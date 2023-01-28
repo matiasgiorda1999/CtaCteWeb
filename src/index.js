bb55673f62466f10c9a71428885547bdb3a84ae8
Mati <matiasgiorda16@gmail.com>
2023-01-28 15:56:13 -0300

    bug fixed

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.css";
 import { Auth0Provider } from "@auth0/auth0-react";
+import locationOf from "./locationOf";
 
 ReactDOM.render(
   <Auth0Provider
     domain={process.env.REACT_APP_AUTH0_DOMAIN}
     clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
     audience={process.env.REACT_APP_AUTH0_API_AUDIENCE}
-    redirectUri={window.location.origin}
+    redirectUri={locationOf() + window.location.origin}
   >
     <App />
   </Auth0Provider>,
