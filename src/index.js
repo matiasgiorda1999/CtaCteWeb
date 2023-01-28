3059da26418f008c844960e62aa1ea9bbdff8d8b
Mati <matiasgiorda16@gmail.com>
2023-01-28 15:57:31 -0300

    bug fixed

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ ReactDOM.render(
     domain={process.env.REACT_APP_AUTH0_DOMAIN}
     clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
     audience={process.env.REACT_APP_AUTH0_API_AUDIENCE}
-    redirectUri={locationOf() + window.location.origin}
+    redirectUri={window.location.origin + locationOf()}
   >
     <App />
   </Auth0Provider>,
